ce3f38c97d55474667f5f27919d5bd86cd6c7093
ranajansenap <er.rashmiranjan84@gmail.com>
2018-06-27 17:08:22 +0530

    step -9 updated .travis folder

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
+#wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+curl https://cli-assets.heroku.com/install.sh | sh
+#heroku login
+#heroku create
+heroku plugins:install @heroku-cli/plugin-container-registry
+
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
